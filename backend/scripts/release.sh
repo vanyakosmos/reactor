e32a5a4b38567963fe27b0ea1ff4a84e0243ee21
Bachynin Ivan <bachynin.i@gmail.com>
2019-07-22 23:40:26 +0300

    fix project dir for gunicord

--- backend/scripts/release.sh
+++ backend/scripts/release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-python ${SCRIPTPATH}/../manage.py migrate
-python ${SCRIPTPATH}/../manage.py collectstatic --noinput -v 0
+BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )/.."
+python ${BASE_DIR}/manage.py migrate
+python ${BASE_DIR}/manage.py collectstatic --noinput -v 0
--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-port=${PORT:-8000}
+PORT=${PORT:-8000}
+BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )/.."
 
 if [[ ${1:-prod} == 'dev' ]]; then
     echo "running DEV server..."
-    exec python manage.py runserver 0.0.0.0:${port}
+    exec python ${BASE_DIR}/manage.py runserver 0.0.0.0:${PORT}
 else
     echo "running PROD server..."
-    exec gunicorn reactor.wsgi --bind 0.0.0.0:${port}
+    exec gunicorn reactor.wsgi --bind 0.0.0.0:${PORT} --chdir ${BASE_DIR}
 fi
